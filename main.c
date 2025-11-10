425f210e286725ec246b7b0f3a227e969f5de0f4
ArthurHenryDEV <arthurdiaspaiva1@gmail.com>
2025-11-10 19:09:49 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 /*
 
-Ex
-
+Escrever um jogo da velha 3x3 em C utilizando todos os conceitos aprendidos até agora. 
+O jogo deve permitir que dois jogadores joguem alternadamente ou que um jogador jogue contra uma máquina.
+O jogo, também, deve contar com sistema de ranking, opção de sair da aplicação, créditos e menu para organização.                        
 
 
 */
