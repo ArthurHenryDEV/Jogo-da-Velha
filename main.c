fee4a8d533e2781442cf7d72c6a10ad5936034b5
ArthurHenryDEV <arthurdiaspaiva1@gmail.com>
2025-11-10 19:11:44 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,8 @@
-/*O projeto consiste em um jogo da velha desenvolvido em linguagem C, com opção de tabuleiro nos
-tamanhos 3x3 ou 4x4. O objetivo principal é permitir partidas entre dois jogadores humanos, entre um
-jogador e o computador, ou até mesmo entre dois computadores. O jogo foi criado com base em princípios
-de programação estruturada, utilizando funções para organizar o código e facilitar sua leitura e manutenção.
+/*
 
-<<<<<<< HEAD
 Escrever um jogo da velha 3x3 em C utilizando todos os conceitos aprendidos atÃ© agora. 
 O jogo deve permitir que dois jogadores joguem alternadamente ou que um jogador jogue contra uma mÃ¡quina.
 O jogo, tambÃ©m, deve contar com sistema de ranking, opÃ§Ã£o de sair da aplicaÃ§Ã£o, crÃ©ditos e menu para organizaÃ§Ã£o.                        
-=======
-O programa apresenta um menu inicial que oferece ao usuário as opções de jogar, visualizar o ranking,
-acessar os créditos ou encerrar o jogo. Ao escolher jogar, o usuário pode definir o modo de jogo
-Ex
-
->>>>>>> d668fa6328f67bf080d1cb3970699e7af26ab46d
-
 
 */
 
