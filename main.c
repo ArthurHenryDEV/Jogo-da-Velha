d668fa6328f67bf080d1cb3970699e7af26ab46d
Abraaohfa <abraaohfaa@gmail.com>
2025-11-10 19:02:55 -0300

    documentation

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
-/*
+/*O projeto consiste em um jogo da velha desenvolvido em linguagem C, com opção de tabuleiro nos
+tamanhos 3x3 ou 4x4. O objetivo principal é permitir partidas entre dois jogadores humanos, entre um
+jogador e o computador, ou até mesmo entre dois computadores. O jogo foi criado com base em princípios
+de programação estruturada, utilizando funções para organizar o código e facilitar sua leitura e manutenção.
 
+O programa apresenta um menu inicial que oferece ao usuário as opções de jogar, visualizar o ranking,
+acessar os créditos ou encerrar o jogo. Ao escolher jogar, o usuário pode definir o modo de jogo
 Ex
 
 
