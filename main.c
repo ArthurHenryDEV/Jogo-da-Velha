08ce872a5ed03fc1d79758a6b912f2f33b4b945b
ArthurHenryDEV <arthurdiaspaiva1@gmail.com>
2025-11-16 18:16:29 -0300

    Função limpar tela adicionada
    Função limpar tela adicionada

--- main.c
+++ main.c
@@ -38,6 +38,9 @@ Créditos/Documentação/Video
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 
 
 struct Jogador {
@@ -51,7 +54,92 @@ struct Ranking {
     int qtd;
 } rankList;
 
+// Função para limpar o terminal (multiplataforma)
+void limparTela() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
+// Pausa a execução de forma multiplataforma (em milissegundos)
+void pausarTela(int milissegundos) {
+#ifdef _WIN32
+    Sleep(milissegundos); // Windows usa milissegundos
+#else
+    usleep(milissegundos * 1000); // usleep usa micro, então multiplica por 1000
+#endif
+}
 
 int main(){
-return 0;
-}
+    int opcao, opcaoTabuleiro;
+
+    do{
+        limparTela();
+
+        printf("===== JOGO DA VELHA =====\n");
+        printf("1 - JOG x JOG\n");
+        printf("2 - JOG x COM\n");
+        printf("0 - Sair\n");
+        printf("==========================\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+
+        if (opcao == 1) {
+            limparTela();
+
+            printf("===== JOGADOR1 vs JOGADOR2 =====\n");
+            printf("1 - Tabuleiro 3x3\n");
+            printf("2 - Tabuleiro 4x4\n");
+            printf("0 - Voltar\n");
+            printf("Escolha uma opcao: ");
+            scanf("%d", &opcaoTabuleiro);
+
+            if (opcaoTabuleiro == 1){
+                printf("Iniciando JOGO 3x3 (JOG 1 vs JOG 2)...\n");
+            }else if (opcaoTabuleiro == 2){
+                printf("Iniciando JOGO 4x4 (JOG 1 vs JOG 2)...\n");
+            }else if (opcaoTabuleiro == 0){
+                printf("Voltando ao menu principal...\n");
+            }else{
+                printf("Opcao invalida!\n");
+            }
+
+            usleep(500000);
+
+        }else if (opcao == 2){
+            limparTela();
+            
+            printf("===== JOGADOR vs COMPUTADOR =====\n");
+            printf("1 - Tabuleiro 3x3\n");
+            printf("2 - Tabuleiro 4x4\n");
+            printf("0 - Voltar\n");
+            printf("Escolha uma opcao: ");
+            scanf("%d", &opcaoTabuleiro);
+
+            if (opcaoTabuleiro == 1){
+                printf("Iniciando JOGO 3x3 (JOG vs COM)...\n");
+            }else if (opcaoTabuleiro == 2){
+                printf("Iniciando JOGO 4x4 (JOG vs COM)...\n");
+            }else if (opcaoTabuleiro == 0){
+                printf("Voltando ao menu principal...\n");
+            }else{
+                printf("Opcao invalida!\n");
+            }
+
+            usleep(500000);
+
+        }else if (opcao == 0){
+            printf("Saindo do jogo...\n");
+
+        }else{
+            printf("Opcao invalida! Tente novamente.\n");
+            usleep(500000);
+        }
+
+    }while (opcao != 0);
+
+    return 0;
+    }
+
